82314642d31f02756e1d9f4e7f0879dd18a41ed7
Dragon2623 <armin1.aliani1@gmail.com>
2022-08-21 11:28:53 +0430

    edit MyServletOne

--- servletTwo/src/main/java/com/servlet/MyServletOne.java
+++ servletTwo/src/main/java/com/servlet/MyServletOne.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 public class MyServletOne extends HttpServlet {
     @Override
@@ -20,12 +21,16 @@ public class MyServletOne extends HttpServlet {
         context.setAttribute("email", "example@example.com");
 
         response.setContentType("text/html");
+
+        String date = "Local date time : " + new Date();
+
         try {
             PrintWriter out = response.getWriter();
             out.println("<h3>Hi there</h3>");
             out.println("<h4>" + country + "</h4>");
             out.println("<head><title>" + title + "</title></head>");
             out.println("<h5>" + title + "</h5>");
+            out.println("<p>" + date + "</p>");
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
